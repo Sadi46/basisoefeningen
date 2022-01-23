e42d898433656f4984680107ae217d42d7d6b98c
Sadi <ditekes@hotmail.com>
2022-01-23 20:48:56 +0100

    How to link a repo to RStudio

---
+++ LinkRepoToRStudio.R
@@ -0,0 +1,31 @@
+# create/fork/clone a repository in Github
+
+# New repo: Name your new repository and give it a short description 
+# Click "Create Repository", 
+# copy the URL for your new repository 
+
+# In RStudio, select New Project, select Version Control, 
+# select Git as your version control software 
+# Paste in the repository URL from before, 
+# select the location where you would like the project stored 
+# When done, click on "Create Project"
+
+# Doing so will initialize a new project linked to the GitHub repository 
+# and open a new session of RStudio
+
+# Create a new R script 
+# Save the file
+
+# in the Git tab of the environment quadrant, you can see the new file  
+
+# Click the checkbox under Staged to stage your file
+# Click on it 
+
+# A new window should open that lists all of the changed files from earlier 
+# and below that shows the differences in the stage files from previous versions 
+# In the upper quadrant, in the message box, write a commit message. 
+# Click Commit and close the window
+
+# push your changes to the GitHub repository, 
+# go to your GitHub repository and see that the commit has been recorded
+

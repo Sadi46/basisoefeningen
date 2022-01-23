e785d86e19bd13d9d757e187e51eae026bcd58fb
Sadi <ditekes@hotmail.com>
2022-01-23 20:21:45 +0100

    How to configure Git to be linked with Github

---
+++ LinkGitGithub.R
@@ -0,0 +1,18 @@
+# configure Git for use with Github
+
+
+# type in the command prompt 
+
+# git config --global user.name "username"  
+# git config --global user.email user-email.com 
+# (same email address you signed up for GitHub with)
+
+# confirm your changes by typing 
+
+# git config --list. 
+
+# If you notice any problems or want to change these values, 
+# just retype the original config commands from earlier with your desired changes
+# exit the command line by typing 
+
+# exit (and hit enter) 

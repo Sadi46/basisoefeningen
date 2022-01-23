8c8817228baf231bace65bb94949007fc53c5ffd
Sadi <ditekes@hotmail.com>
2022-01-23 18:53:51 +0100

    added tutorial

--- Scripts/versionControl.R
+++ Scripts/versionControl.R
@@ -1,1 +1,39 @@
-# how to link existing project to github
+# how to link an existing project to github
+
+# Open Git Bash or Terminal and navigate to the directory 
+# containing your project files. 
+# Move around directories by 
+
+# type
+# CD for change directory, followed by the path of the directory.
+
+# When the command prompt in the line before $ says the correct location
+# of your project, you are in the correct location. 
+
+# type 
+# git init 
+# git add. (with .!) 
+
+# This Initializes this directory as a Git repository 
+# and Stages / adds all of the files in the directory to your local repository. 
+# Commit these changes to the Git repository using 
+
+# git commit -m "Initial Commit" 
+
+# At this point, we have an R project linked to Git version control. 
+# To link it to Github, 
+
+# go to github.com
+
+# Create a new repository with the same name
+# note: do not initialize the readme, gitignore or license. 
+
+# Once you've created this repository, 
+# there is an option to push an existing repository from the command line 
+# with instructions below containing code on how to do so.
+
+# In Git Bash or Terminal, copy and paste these lines of code 
+# After doing so, refresh your GitHub page 
+
+# you should now have access to the Git tab in the upper right quadrant
+# then you can push to GitHub from within RStudio any future changes. 
